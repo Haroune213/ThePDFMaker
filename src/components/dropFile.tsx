d64e0e54a16dccc97f9445e9abedc9d4311f95fc
haroune213 <123390922+Haroune213@users.noreply.github.com>
2024-02-23 15:31:38 -0500

    displaying the dropped elements

--- src/components/dropFile.tsx
+++ src/components/dropFile.tsx
@@ -1,5 +1,6 @@
 import "./../App.css"
 import drop_icon from "../assets/drop.png"
+import pdf_icon from "../assets/pdf.png"
 import { useState,useRef } from "react"
 
 
@@ -7,16 +8,81 @@ import { useState,useRef } from "react"
 export default function DropFile(){
     const[files, setFiles]=  useState<any | null>(null);
     const inputRef = useRef<HTMLInputElement>(null);
+    const [imgElements, setImgElements] = useState<(JSX.Element | null)[]>([]);
 
-    const handleDragOver=(event:any)=>{
+    function handleFileType(file: File): number {
+        const fileType = file.type;
+        switch(fileType){
+            case 'application/pdf':
+                return 0;
+            case 'image/png':
+                return 1;
+            case 'image/jpeg':
+                return 1;
+            default:
+                return -1;
+        }
+    }
+
+
+    const handleDragOver=(event:any)=>{ 
         event.preventDefault()
     }
 
-    const handleDrop=(event:any)=>{
+    
+    const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault()
-        console.log(event.dataTransfer.files)
-        setFiles(event.dataTransfer.files)
-        alert(files)
+
+        if (event.dataTransfer.files) {
+            const newImgElements: (JSX.Element | null)[] = [];
+            Array.from(event.dataTransfer.files).forEach((file, index) => {
+                const fileType = handleFileType(file);
+                switch (fileType) {
+                    case 0:
+                        newImgElements.push(<img key={index} src={pdf_icon} draggable="true" alt="PDF" />);
+                        break;
+                    case 1:
+                        const reader = new FileReader();
+                        reader.onload = () => {
+                            const imgData = reader.result as string;
+                            newImgElements.push(<img key={index} src={imgData} draggable="true" />);
+                            setImgElements([...newImgElements]);
+                            console.log('processed')
+                        };
+                        reader.readAsDataURL(file);
+                        break;
+                        break;
+                }
+            });
+            setFiles(event.dataTransfer.files);
+        }
+    };
+
+    const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            const selectedFiles = event.target.files;
+            setFiles(selectedFiles);
+            const newImgElements: (JSX.Element | null)[] = [];
+            Array.from(selectedFiles).forEach((file, index) => {
+                const fileType = handleFileType(file);
+                switch (fileType) {
+                    case 0:
+                        newImgElements.push(<img key={index} src={pdf_icon} draggable="true" alt="PDF" />);
+                        break;
+                    case 1:
+                        const reader = new FileReader();
+                        reader.onload = () => {
+                            const imgData = reader.result as string;
+                            newImgElements.push(<img key={index} src={imgData} draggable="true"/>);
+                            setImgElements([...newImgElements]);
+                        };
+                        reader.readAsDataURL(file);
+                        break;
+                    default:
+                        break;
+                }
+            });
+        }
     }
 
 
@@ -24,16 +90,19 @@ export default function DropFile(){
         <div 
             onDragOver={handleDragOver}
             onDrop={handleDrop}
-            className="m-5 text-white w-[60vw] h-[400px] bg-black bg-opacity-[40%] rounded-xl border-slate-100 border-dashed border-2 hover:border-slate-500 border-spacing-1 transition-all shadow-xl shadow-slate-1000 flex items-center justify-center flex-col"
+            className="m-5 text-white w-[60vw] h-[400px] bg-black bg-opacity-[40%] rounded-xl border-slate-100 border-dashed border-2 hover:border-slate-500 border-spacing-1 transition-all shadow-xl shadow-slate-1000 flex items-center justify-center flex-col "
         >
             {files ? (
-                <div>
-                    <ul>
-                        {Array.from(files).map((file: any, id: number) => (
-                            <li key={id}>
-                                {file.name}
-                            </li>
-                        ))}
+                <div className="w-full h-full p-5 bg-[rgba(0,0,0,0.1)] transition-all">
+                    <ul className="w-full h-full p-5 grid grid-cols-4 gap-8 no-scrollbar overflow-scroll overflow-x-hidden">
+                        {Array.from(files).map((file: any, id: number) => {
+                            return (
+                                <li key={id} className="flex flex-col items-center justify-center break-all">
+                                    {imgElements[id]} {/* Render the corresponding imgElement */}
+                                    {file.name}
+                                </li>
+                            );
+                        })}
                     </ul>
                 </div>
             ) : (
@@ -44,7 +113,7 @@ export default function DropFile(){
                     <input 
                         type="file"
                         multiple 
-                        onChange={(event) => setFiles(event.target.files)}
+                        onChange={handleFileInputChange}
                         hidden
                         ref={inputRef}
                     />
