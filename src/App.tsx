c1d13494c037d2f379dc832efbc2e3ef7772a29e
haroune213 <123390922+Haroune213@users.noreply.github.com>
2024-02-22 21:52:37 -0500

    updating the dropFIle inner content

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Navbar from './components/navbar'
 
 function App() {
 
+  //for home     THE PDF<span className='block bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500  text-transparent bg-clip-text'>Maker</span>
+
   return (
     <div className='w-[100vw] h-[100vh] flex items-center flex-col'>
     <Navbar />  
     <h1 className='text-7xl text-slate-100 font-bold pt-3 text-center header-leading'>
-    THE PDF<span className='block bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500  text-transparent bg-clip-text'>Maker</span>
+    Merge your <span className=' bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500  text-transparent bg-clip-text'>PDF's</span> 
     </h1>
     <DropFile />
     </div>
--- src/components/dropFile.tsx
+++ src/components/dropFile.tsx
@@ -1,10 +1,14 @@
 import "./../App.css"
+import drop_icon from "../assets/drop.png"
 
 export default function DropFile(){
 
     return(
-        <div className="m-5  text-white w-[60vw] h-[400px] bg-black bg-opacity-[40%] rounded-xl border-salte-100 border-dashed border-2  hover:border-slate-500 border-spacing-1 transition-all shadow-xl shadow-slate-1000 flex items-center justify-center">
-            hey
+        <div className="m-5  text-white w-[60vw] h-[400px] bg-black bg-opacity-[40%] rounded-xl border-slate-100 border-dashed border-2  hover:border-slate-500 border-spacing-1 transition-all shadow-xl shadow-slate-1000 flex items-center justify-center flex-col">
+            <img src={drop_icon} className="w-[200px] p-0" draggable="false"/>
+            <h2 className="text-5xl font-bold">Drop your files here</h2>
+            <span className="text-gray-300 font-medium">upload only PDF files</span>
+            <button className="px-8 py-3 rounded-lg bg-blue-800 hover:bg-blue-700 transition-all mt-5">Upload Image</button>
         </div>
     )
 } 
